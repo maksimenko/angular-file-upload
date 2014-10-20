1b1134980ae8b1689e651af2055f6d01cd45d82e
Victor H. Maksimenko <victor@spokoy.com>
2014-10-20 12:42:15 -0430

    fix for angular 1.3 $http transformResponse changes (add a header getter fn)

--- angular-file-upload.js
+++ angular-file-upload.js
@@ -398,14 +398,25 @@ module
                 return (status >= 200 && status < 300) || status === 304;
             };
             /**
+             * Returns a function lookup for header values by name
+             * @param {headers} headers objet as returned by _parseHeaders
+             * @returns {Function}
+             * @private
+             */
+            FileUploader.prototype._getHeaderFn = function (headers) {
+                return function (name) {
+                    return headers[angular.lowercase(name)] || null;
+                };
+            };
+            /**
              * Transforms the server response
              * @param {*} response
              * @returns {*}
              * @private
              */
-            FileUploader.prototype._transformResponse = function(response) {
+            FileUploader.prototype._transformResponse = function(response, headersFn) {
                 angular.forEach($http.defaults.transformResponse, function(transformFn) {
-                    response = transformFn(response);
+                    response = transformFn(response, headersFn);
                 });
                 return response;
             };
@@ -467,7 +478,7 @@ module
 
                 xhr.onload = function() {
                     var headers = that._parseHeaders(xhr.getAllResponseHeaders());
-                    var response = that._transformResponse(xhr.response);
+                    var response = that._transformResponse(xhr.response, that._getHeaderFn(headers));
                     var gist = that._isSuccessCode(xhr.status) ? 'Success' : 'Error';
                     var method = '_on' + gist + 'Item';
                     that[method](item, response, xhr.status, headers);
@@ -476,14 +487,14 @@ module
 
                 xhr.onerror = function() {
                     var headers = that._parseHeaders(xhr.getAllResponseHeaders());
-                    var response = that._transformResponse(xhr.response);
+                    var response = that._transformResponse(xhr.response, that._getHeaderFn(headers));
                     that._onErrorItem(item, response, xhr.status, headers);
                     that._onCompleteItem(item, response, xhr.status, headers);
                 };
 
                 xhr.onabort = function() {
                     var headers = that._parseHeaders(xhr.getAllResponseHeaders());
-                    var response = that._transformResponse(xhr.response);
+                    var response = that._transformResponse(xhr.response, that._getHeaderFn(headers));
                     that._onCancelItem(item, response, xhr.status, headers);
                     that._onCompleteItem(item, response, xhr.status, headers);
                 };
@@ -549,8 +560,8 @@ module
                     } catch (e) {}
 
                     var xhr = {response: html, status: 200, dummy: true};
-                    var response = that._transformResponse(xhr.response);
                     var headers = {};
+                    var response = that._transformResponse(xhr.response, function () { return null });
 
                     that._onSuccessItem(item, response, xhr.status, headers);
                     that._onCompleteItem(item, response, xhr.status, headers);
